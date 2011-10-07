dee82f3c1084245ff2be8921e0d92f3901251fc4
Ian Oxley <ijoxley@gmail.com>
2011-10-07 14:18:13 +0100

    Completed about_triangle_project.rb

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,22 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  return :equilateral if is_equilateral?(a, b, c)
+  return :isosceles if is_isosceles?(a, b, c)
+  return :scalene if is_scalene?(a, b, c)
+  raise "Unknown triangle"
+end
+
+def is_equilateral?(a, b, c)
+  a == b and b == c
+end
+
+def is_isosceles?(a, b, c)
+  (a == b and a != c) or (a == c and a != b) or (b == c and b != a)
+end
+
+def is_scalene?(a, b, c)
+  a != b and b != c and a != c
 end
 
 # Error class used in part 2.  No need to change this code.
